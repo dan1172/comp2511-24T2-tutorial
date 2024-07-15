83a74bfaf653cb3ea2df7854f762f3ec9499b87c
Daniel <z5417222@ad.unsw.edu.au>
2024-07-16 00:23:28 +1000

    add generics stuff

---
+++ tute08/src/example/GenericsExamples.java
@@ -0,0 +1,45 @@
+package example;
+
+import java.util.*;
+
+public class GenericsExamples {
+    public static void main(String[] args) {
+        /**
+         * public class A {}
+         * public class B extends A{}
+         * public class C extends B{}
+         */
+        // List<? super B> foo1 = new ArrayList<>();
+        // foo1.add(new A());
+        // foo1.add(new B());
+        // foo1.add(new C());
+        // foo1 = new ArrayList<A>();
+        // foo1 = new ArrayList<B>();
+        // foo1 = new ArrayList<C>();
+
+        // when do we use <? super T> then?
+        // https://docs.oracle.com/javase/8/docs/api/java/util/stream/Stream.html
+        // filter(Predicate<? super T> predicate)
+
+        // imagine we have a list of strings we want to filter
+        // what types of predicates do we want to accept?
+
+
+        // List<? extends Number> foo2 = new ArrayList<Number>();
+        // foo2.add(5); // why can't we add to the list?
+        // foo2 could be a list of numbers, ints, floats
+        // when would we use <? extends Number> then?
+    }
+}
+
+/**
+ * Can we have wildcards in classes?
+ * class GenericsExamples <?> {}
+ * class GenericsExamples <? extends A> {}
+ * class GenericsExamples <? super A> {}
+ * 
+ * class GenericsExamples <E> {}
+ * class GenericsExamples <E extends A> {}
+ * class GenericsExamples <E super A> {}
+ * 
+ */
